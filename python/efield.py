ea7c0aed8e036296be58c302142efc527e6f54f8
smfarzaneh <smf552@nyu.edu>
2020-08-14 15:23:54 -0400

    generate k points around the K point

--- python/efield.py
+++ python/efield.py
@@ -19,3 +19,25 @@ class Efield():
             f.write(s)
         f.write(" )")
         f.close()
+
+    @classmethod
+    def print_kpoints(self, direction='x', num_k=201):
+        relpath = '../in/k' + direction + '.val'
+        current = os.path.dirname(__file__)
+        abspath = os.path.join(current, relpath)
+        if os.path.isfile(abspath):
+            f = open(abspath, "w")
+        else:
+            f = open(abspath, "x")
+        f.write("#!/bin/sh\nk_string=\"")
+        K_point_x = 2.0/3.0
+        k_vals = np.linspace(-0.5, 0.5, num_k, endpoint=True)        
+        f.write(str(num_k) + "\n")
+        for k in k_vals:
+            if direction=='x':
+                s = " {0:.8f}  {1:.8f}  {2:.8f}  1.0\n".format(k + K_point_x, 0.0, 0.0)
+            else:
+                s = " {0:.8f}  {1:.8f}  {2:.8f}  1.0\n".format(K_point_x, k, 0.0)
+            f.write(s)
+        f.write("\"")
+        f.close()
